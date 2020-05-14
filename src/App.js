cbccea3021a1e6dab592c4ccde39082acedb4b78
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-05-14 19:42:54 +0600

    add break in header

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export function App() {
         <a href="https://netlify.com" target="_blank" rel="noopener noreferrer">
           Learn Netlify
         </a>
+        <br />
         <a
           href="https://sass-lang.com"
           target="_blank"
