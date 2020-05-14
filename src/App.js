d52c22ab5fdc2e99b27668c9aa27e84038143c7e
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-05-14 19:08:10 +0600

    clean up stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './app.css'
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <h1>Henlo!</h1>
+      </main>
     </div>
-  );
+  )
 }
-
-export default App;
